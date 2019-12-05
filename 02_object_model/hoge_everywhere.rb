0b81f41078c3edb88b05034a38ed82f37b32c882
mserizawa <sasa.serizawa@gmail.com>
2019-12-05 14:29:12 +0900

    solve

--- 02_object_model/hoge_everywhere.rb
+++ 02_object_model/hoge_everywhere.rb
@@ -6,3 +6,45 @@
 # - Class
 # - Hash
 # - TrueClass
+
+class String
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Integer
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Number
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Class
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Hash
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class TrueClass
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Float
+  def hoge
+    Hoge::Hoge
+  end
+end
